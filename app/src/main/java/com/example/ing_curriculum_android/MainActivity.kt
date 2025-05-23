5fd274d241c93374ecdf4d1aacd9e4a692d49086
benjaluduena <benjaminludluq@gmail.com>
2025-05-23 15:19:54 -0300

    first commit

---
+++ app/src/main/java/com/example/ing_curriculum_android/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ing_curriculum_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btnMasInfo).setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+    }
+}
