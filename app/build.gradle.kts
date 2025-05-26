613908c4ac2b40b72cb5f1c7b5fb486b6d36168a
benjaluduena <benjaminludluq@gmail.com>
2025-05-26 18:26:02 -0300

    Hice la imagen redondeada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.material) // si ya lo tienes en el catálogo
+    implementation("com.google.android.material:material:1.9.0") // sin +
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
